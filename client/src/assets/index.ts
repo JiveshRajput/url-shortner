8494267380a627167a4676206fc9e2068fde9605
Jivesh <jivesh.rajput@seeidinc.com>
2024-08-14 23:15:30 +0530

    feat(client): Added icons

---
+++ client/src/assets/index.ts
@@ -0,0 +1,8 @@
+export const IMAGES = {
+  AUTH_SIGN_IN: require('./svg/auth-sign-in.svg'),
+  AUTH_SIGN_UP: require('./svg/auth-sign-up.svg'),
+  AUTH_FORGOT_PASSWORD: require('./svg/auth-forgot-password.svg'),
+  AUTH_RESET_PASSWORD: require('./svg/auth-reset-password.svg'),
+  AUTH_IMAGE_UPLOAD: require('./svg/auth-upload-image.svg'),
+  AUTH_USER_VALIDATED: require('./svg/auth-user-verified.svg'),
+};
