6d4671790e2a020cbc3c47d500aab0ca574cb870
Jivesh <jivesh.rajput@seeidinc.com>
2024-09-09 08:05:11 +0530

    fix(client): Updated middleware file authentication logic

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,39 +3,37 @@ import { NextResponse } from 'next/server';
 import { authenticateUserApi } from './features/auth/api';
 import { COOKIES } from './features/auth/constants';
 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: [
-    '/dashboard/:path*', 
-    '/verify-otp'
-  ],
-};
-
-
-
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest, context: NextFetchEvent) {
   console.log('middleware starts');
-  // context.waitUntil()
-  try {
-    const response = await authenticateUserApi();
-    const data = await response.json();
-    console.log('middleware authenticate user', data);
+  context.waitUntil(
+    authenticateUserApi()
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('middleware authenticate user', data);
 
-    if (data.statusCode === 401) {
-      const response = NextResponse.redirect(new URL('/sign-in', request.url));
-      response.cookies.delete(COOKIES.ACCESS_TOKEN);
-      response.cookies.delete(COOKIES.REFRESH_TOKEN);
-      response.cookies.delete(COOKIES.USER_ID);
-      return response;
-    }
-    console.log('middleware ends');
-    return NextResponse.next();
-  } catch (error) {
-    const response = NextResponse.redirect(new URL('/sign-in', request.url));
-    response.cookies.delete(COOKIES.ACCESS_TOKEN);
-    response.cookies.delete(COOKIES.REFRESH_TOKEN);
-    response.cookies.delete(COOKIES.USER_ID);
-    return response;
-  }
+        if (data.statusCode === 401) {
+          const response = NextResponse.redirect(new URL('/sign-in', request.url));
+          response.cookies.delete(COOKIES.ACCESS_TOKEN);
+          response.cookies.delete(COOKIES.REFRESH_TOKEN);
+          response.cookies.delete(COOKIES.USER_ID);
+          return response;
+        }
+      })
+      .catch((error) => {
+        const response = NextResponse.redirect(new URL('/sign-in', request.url));
+        response.cookies.delete(COOKIES.ACCESS_TOKEN);
+        response.cookies.delete(COOKIES.REFRESH_TOKEN);
+        response.cookies.delete(COOKIES.USER_ID);
+        return response;
+      }),
+  );
+  console.log('middleware ends');
+
+  return NextResponse.next();
 }
+
+// See "Matching Paths" below
+export const config = {
+  matcher: ['/dashboard/:path*', '/verify-otp'],
+};
